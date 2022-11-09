e90b0761271b07b0f44f1babb25a04f1a8e3f460
akshata_rn <akshatarn123@gamail.com>
2022-11-09 10:22:19 +0530

    Modified Employee Attendence code

--- EmployeeWageDemo/EmployeeWageDemo.cs
+++ EmployeeWageDemo/EmployeeWageDemo.cs
@@ -11,8 +11,19 @@ namespace EmployeeWageDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Computation");
-            Console.ReadLine();
-
+            int empPresent = 1;
+            Random r = new Random();
+            int empInput = r.Next(0, 10);
+            if(empInput == empPresent)
+            {
+                Console.WriteLine("Employee is Present");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                Console.ReadLine();
+            }
         }
     }
 }
