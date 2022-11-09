509c7d36b6adef0f332f603e159c9d3918ac1316
akshata_rn <akshatarn123@gamail.com>
2022-11-09 15:18:36 +0530

    Modified using switch case statement

--- EmployeeWageDemo/EmployeeWageDemo.cs
+++ EmployeeWageDemo/EmployeeWageDemo.cs
@@ -12,26 +12,28 @@ namespace EmployeeWageDemo
         {
             Console.WriteLine("Welcome to Employee Computation");
             const int EMP_RATE_HR=20;
-            int FullTime = 1;
-            int PartTime = 2;
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
             int empWage = 0;
             int empHrs = 0;
             Random r = new Random();
             int empInput = r.Next(0, 3);
-            if (FullTime == empInput)
+            switch(empInput)
             {
-                Console.WriteLine("Fulltime Employee is Present");
-                empHrs = 8;
-            }
-            else if(PartTime == empInput)
-            {
-                Console.WriteLine("Parttime Employee is Present");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-                empHrs = 0;
+                case FULL_TIME:
+                    Console.WriteLine("Fulltime Employee is Present");
+                    empHrs = 8;
+                    break;
+                case PART_TIME:
+                    Console.WriteLine("Parttime Employee is Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is absent");
+                    empHrs = 0;
+                    break;
+
+
             }
             empWage = EMP_RATE_HR * empHrs;
             Console.WriteLine("DAily Employee wage is:" + empWage);
